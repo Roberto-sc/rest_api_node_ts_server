d4deb5519bdb7d7a7a37c476303229ab07a99a1a
Roberto-sc <luis.solano@ucrso.info>
2024-05-05 12:50:37 -0600

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.suma = void 0;
+function suma() {
+    return 2 + 5;
+}
+exports.suma = suma;
+//# sourceMappingURL=server.js.map
